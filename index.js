007fe7759a5d4606a651b7db85fb37f891d4cb7b
andrepolischuk <andre.polischuk@gmail.com>
2015-05-18 11:54:24 +0300

    Extract modules

--- index.js
+++ index.js
@@ -2,6 +2,18 @@
 'use strict';
 
 /**
+ * Module dependencies
+ */
+
+var keycode = require('keycodes');
+
+try {
+  var events = require('event');
+} catch (err) {
+  var events = require('component-event');
+}
+
+/**
  * Combinations
  */
 
@@ -18,84 +30,6 @@ var modifiers = {
   option : 18,
   shift : 16
 };
-
-/**
- * Special keys
- */
-
-var special = {
-  escape : 27,
-  esc : 27,
-  '`' : 192,
-  '-' : 189,
-  '=' : 187,
-  backspace : 8,
-  tab : 9,
-  '\\' : 220,
-  '[' : 219,
-  ']' : 221,
-  ';' : 186,
-  '\'' : 222,
-  enter : 13,
-  'return' : 13,
-  ',' : 188,
-  '.' : 190,
-  '/' : 191,
-  space : 32,
-  pause : 19,
-  insert : 45,
-  'delete' : 46,
-  home : 36,
-  end : 35,
-  pageup : 33,
-  pagedown : 34,
-  left : 37,
-  up: 38,
-  right : 39,
-  down : 40
-};
-
-/**
- * Function keys
- */
-
-for (var i = 1; i < 20; i++) {
-  special['f' + i] = 111 + i;
-}
-
-/**
- * Get key code
- *
- * @param  {String} key
- * @return {String}
- * @api private
- */
-
-function code(key) {
-  return special[key.toLowerCase()] || key.toUpperCase().charCodeAt(0);
-}
-
-/**
- * Get key name
- *
- * @param  {Number} code
- * @return {String}
- * @api private
- */
-
-function find(code) {
-
-  var res;
-
-  for (var k in special) {
-    if (special.hasOwnProperty(k)) {
-      res = special[k] === code ? k : res;
-    }
-  }
-
-  return res || String.fromCharCode(code).toLowerCase();
-
-}
 
 /**
  * Find context position
@@ -145,32 +79,11 @@ function detach(e) {
   e = e || {};
 
   if (e.keyCode) {
-    delete attached.keys[find(e.keyCode)];
+    delete attached.keys[keycode(e.keyCode)];
   } else {
     attached.target = {};
     attached.keys = {};
     attached.fn = attached.fn || null;
-  }
-
-}
-
-/**
- * Add event listener
- *
- * @param {Object} target
- * @param {String} event
- * @param {Function} fn
- * @api private
- */
-
-function onEventListener(target, event, fn) {
-
-  if (target.addEventListener) {
-    target.addEventListener(event, fn, false);
-  } else {
-    target.attachEvent('on' + event, function() {
-      fn.call(target, window.event);
-    });
   }
 
 }
@@ -248,8 +161,7 @@ function clone(comb) {
  */
 
 function listener(e) {
-
-  var key = find(e.keyCode);
+  var key = keycode(e.keyCode);
 
   if (e.keyCode > 15 && e.keyCode < 19) {
     return;
@@ -319,7 +231,7 @@ function parseString(string) {
       res.ctrlKey = res.ctrlKey || string[k] === 'ctrl' || string[k] === 'control';
       res.shiftKey = res.shiftKey || string[k] === 'shift';
     } else {
-      res.keys[string[k]] = code(string[k]);
+      res.keys[string[k]] = keycode(string[k]);
     }
 
   }
@@ -474,9 +386,9 @@ ks.removeContext = function(name) {
  * Listener initialization
  */
 
-onEventListener(document, 'keydown', listener);
-onEventListener(document, 'keyup', detach);
-onEventListener(window, 'blur', detach);
+events.bind(document, 'keydown', listener);
+events.bind(document, 'keyup', detach);
+events.bind(window, 'blur', detach);
 
 /**
  * Module exports
