4b66573d57a4139bcbf07311df4d979bc35222f3
andrepolischuk <andre.polischuk@gmail.com>
2015-07-30 13:23:28 +0300

    Tweaks

--- index.js
+++ index.js
@@ -1,27 +1,7 @@
-
 'use strict';
-
-/**
- * Module dependencies
- */
-
-var keycomb = require('keycomb');
 var events = require('event');
-
-/**
- * Expose dispatcher
- */
-
+var keycomb = require('keycomb');
 module.exports = ks;
-
-/**
- * Dispatcher
- *
- * @param {String} keys
- * @param {Function} fn
- * @param {String} scope
- * @api public
- */
 
 function ks(keys, fn, scope) {
   if (typeof keys === 'function') return ks('*', keys, fn);
@@ -33,46 +13,14 @@ function ks(keys, fn, scope) {
   }
 }
 
-/**
- * Expose callbacks
- */
-
 ks.callbacks = [];
-
-/**
- * Expose current
- */
-
-ks.current = {};
-ks.current.keyCode = [];
-
-/**
- * Expose scopes
- */
-
+ks.current = {keyCode: []};
 ks.scopes = [];
-
-/**
- * Show defined context
- *
- * @param {String} keys
- * @param {Object} event
- * @api public
- */
 
 ks.show = function(keys, event) {
   var ctx = new Context(keys, event);
   execute(ctx);
 };
-
-/**
- * Detach combination
- *
- * @param {String} keys
- * @param {Function} fn
- * @param {String} scope
- * @api public
- */
 
 ks.remove = function(keys, fn, scope) {
   for (var i = 0, route, len = ks.callbacks.length; i < len; i++) {
@@ -84,26 +32,10 @@ ks.remove = function(keys, fn, scope) {
   }
 };
 
-/**
- * Attach scope
- *
- * @param  {String} name
- * @return {Array}
- * @api public
- */
-
 ks.scope = function(name) {
   if (ks.scopes.indexOf(name) < 0) ks.scopes.push(name);
   return ks.scopes;
 };
-
-/**
- * Detach scope
- *
- * @param  {String} name
- * @return {Array}
- * @api public
- */
 
 ks.removeScope = function(name) {
   if (name) {
@@ -111,28 +43,17 @@ ks.removeScope = function(name) {
   } else {
     ks.scopes = [];
   }
+
   return ks.scopes;
 };
-
-/**
- * Start listener
- */
 
 events.bind(document, 'keydown', attach);
 events.bind(document, 'keyup', detach);
 events.bind(window, 'blur', detach);
 
-/**
- * Key attach
- *
- * @param {Object} event
- * @api private
- */
-
 function attach(event) {
   var cur = ks.current;
   event = event || window.event;
-
   if (!event.target) event.target = event.srcElement;
   if (event.keyCode > 15 && event.keyCode < 19) return;
 
@@ -146,25 +67,11 @@ function attach(event) {
   ks.show(keycomb(cur).join('+'), event);
 }
 
-/**
- * Key detach
- *
- * @param {Object} event
- * @api private
- */
-
 function detach(event) {
   var cur = ks.current;
   event = event || window.event;
   cur.keyCode.splice(cur.keyCode.indexOf(event.keyCode), 1);
 }
-
-/**
- * Execute context
- *
- * @param {Object} ctx
- * @api private
- */
 
 function execute(ctx) {
   var i = 0;
@@ -180,14 +87,6 @@ function execute(ctx) {
   next();
 }
 
-/**
- * Context
- *
- * @param {String} keys
- * @param {Element} event
- * @api private
- */
-
 function Context(keys, event) {
   this.keys = keys;
   this.event = keycomb(keys);
@@ -200,15 +99,6 @@ function Context(keys, event) {
   } : function() {};
 }
 
-/**
- * Route
- *
- * @param {String} keys
- * @param {Function} fn
- * @param {String} scope
- * @api private
- */
-
 function Route(keys, fn, scope) {
   this.keys = keys;
   this.event = keys === '*' ? keys : keycomb(keys);
@@ -216,36 +106,20 @@ function Route(keys, fn, scope) {
   this.scope = scope;
 }
 
-/**
- * Route callback
- *
- * @return {Function}
- * @api private
- */
-
 Route.prototype.callback = function() {
   var self = this;
+
   return function(ctx, next) {
     if (self.match(ctx)) return self.fn(ctx, next);
     next();
   };
 };
 
-/**
- * Route match
- *
- * @param  {Object} ctx
- * @return {Boolean}
- * @api private
- */
-
 Route.prototype.match = function(ctx) {
   var a = this.event;
   var b = ctx.event;
-
   if (this.scope && ctx.scope.indexOf(this.scope) < 0) return;
   if (a === '*') return true;
-
   var len = a.keyCode.length;
   if (len !== b.keyCode.length) return;
 
